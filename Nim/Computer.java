5d39d0d85d817e1ae4ac4d5a89ef472363261474
megankirschbaum <mkirschbaum7750@student.palomar.edu>
2025-03-26 13:04:01 -0700

    updated computer class
    added a method thats supposed to calculate the optimal move

--- Nim/Computer.java
+++ Nim/Computer.java
@@ -1,3 +1,15 @@
 public class Computer extends Player{
-    
+    public int calculateMove(int tiles) {
+        int target = 1;
+        int power = -1;
+        while(target - 1 < tiles) {
+            target *= 2;
+            power++;
+        }
+        if(tiles - Math.pow(2, power) + 1 == 0) {
+            return 1;
+        } else {
+            return tiles - Math.pow(2, power) + 1;
+        }
+    }
 }
